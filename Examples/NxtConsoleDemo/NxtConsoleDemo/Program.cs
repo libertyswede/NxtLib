83067c9bddf2600114fcebf94a8ebfe4f1afa416
Liberty Swede <libertyswede@outlook.com>
2015-01-10 21:30:02 +0100

    Added console demo for getBalance and sendMoney

---
+++ Examples/NxtConsoleDemo/NxtConsoleDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using NxtLib.AccountOperations;
+
+namespace NxtConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nxtManager = new NxtManager(new AccountService());
+            Task.WaitAll(nxtManager.Start());
+        }
+    }
+}
