9c85cf20af5823239fe91f19b50415110e3efc72
Liberty Swede <libertyswede@outlook.com>
2015-01-11 21:55:21 +0100

    Added missing files

---
+++ NxtLib/Appendix.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NxtLib
+{
+    public abstract class Appendix : Attachment
+    {
+        protected Appendix(IReadOnlyDictionary<string, object> values, string name) 
+            : base(values, name)
+        {
+        }
+    }
+
+    public class UnencryptedMessage : Appendix
+    {
+        public bool MessageIsText { get; set; }
+        public string Message { get; set; }
+
+        internal const string AttachmentName = "version.Message";
+
+        public UnencryptedMessage(IReadOnlyDictionary<string, object> values)
+            : base(values, AttachmentName)
+        {
+            MessageIsText = Convert.ToBoolean(values["messageIsText"]);
+            Message = values["message"].ToString();
+        }
+    }
+
+    public abstract class EncryptedMessageBase : Appendix
+    {
+        public bool IsText { get; set; }
+        public string Nonce { get; set; }
+        public string Data { get; set; }
+
+        protected EncryptedMessageBase(IReadOnlyDictionary<string, object> values, string encryptedMessageKey, string name)
+            : base(values, name)
+        {
+            var encryptedMessageValues = (IReadOnlyDictionary<string, object>)values[encryptedMessageKey];
+
+            IsText = (bool)encryptedMessageValues["isText"];
+            Nonce = encryptedMessageValues["nonce"].ToString();
+            Data = encryptedMessageValues["data"].ToString();
+        }
+    }
+
+    public class EncryptedMessage : EncryptedMessageBase
+    {
+        internal const string AttachmentName = "version.EncryptedMessage";
+
+        public EncryptedMessage(IReadOnlyDictionary<string, object> values)
+            : base(values, "encryptedMessage", AttachmentName)
+        {
+        }
+    }
+
+    public class EncryptToSelfMessage : EncryptedMessageBase
+    {
+        internal const string AttachmentName = "version.EncryptToSelfMessage";
+
+        public EncryptToSelfMessage(IReadOnlyDictionary<string, object> values)
+            : base(values, "encryptToSelfMessage", AttachmentName)
+        {
+        }
+    }
+
+    public class PublicKeyAnnouncement : Attachment
+    {
+        public string RecipientPublicKey { get; set; }
+
+        internal const string AttachmentName = "version.PublicKeyAnnouncement";
+
+        public PublicKeyAnnouncement(IReadOnlyDictionary<string, object> values)
+            : base(values, AttachmentName)
+        {
+            RecipientPublicKey = values["recipientPublicKey"].ToString();
+        }
+    }
+}
---
+++ NxtLib/Internal/TransactionConverter.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace NxtLib.Internal
+{
+    /// <summary>
+    /// A converter is needed for whole Transaction class in order to split the attachment into different .NET properties 
+    /// (atm. Attachment, Message, EncryptedMessage, EncryptToSelfMessage & PublicKeyAnnouncement atm.)
+    /// </summary>
+    internal class TransactionConverter : JsonConverter
+    {
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (objectType != typeof(Transaction))
+            {
+                throw new ArgumentException("Can only convert to transaction object");
+            }
+
+            if (reader.TokenType != JsonToken.StartObject)
+            {
+                throw new ArgumentException("Invalid token type, must be StartObject");
+            }
+
+            var jObject = JObject.Load(reader);
+            var attachmentConverter = new AttachmentConverter(jObject.SelectToken("attachment") as JObject);
+
+            var transaction = new Transaction();
+            transaction.Amount = GetValueOrDefault(jObject, "amountNQT", obj => Amount.CreateAmountFromNqt(Convert.ToInt64(obj)));
+            transaction.Attachment = attachmentConverter.GetAttachment();
+            transaction.BlockId = GetValueOrNull(jObject, "block", Convert.ToUInt64);
+            transaction.BlockTimestamp = GetDateTimeOrNull(jObject, "blockTimestamp");
+            transaction.Confirmations = GetValueOrNull(jObject, "confirmations", Convert.ToInt32);
+            transaction.Deadline = GetValueOrDefault(jObject, "deadline", Convert.ToInt16);
+            transaction.EcBlockId = GetValueOrDefault(jObject, "ecBlockId", Convert.ToUInt64);
+            transaction.EcBlockHeight = GetValueOrDefault(jObject, "ecBlockHeight", Convert.ToInt32);
+            transaction.EncryptedMessage = attachmentConverter.GetEncryptedMessage();
+            transaction.EncryptToSelfMessage = attachmentConverter.GetEncryptToSelfMessage();
+            transaction.FeeNqt = GetValueOrDefault(jObject, "feeNQT", obj => Amount.CreateAmountFromNqt(Convert.ToInt64(obj)));
+            transaction.FullHash = GetValueOrDefault(jObject, "fullHash", obj => obj.ToString());
+            transaction.Height = GetValueOrDefault(jObject, "height", Convert.ToInt32);
+            transaction.Message = attachmentConverter.GetMessage();
+            transaction.Recipient = GetValueOrNull(jObject, "recipient", Convert.ToUInt64);
+            transaction.RecipientRs = GetValueOrDefault(jObject, "recipientRS", obj => obj.ToString());
+            transaction.ReferencedTransactionFullHash = GetValueOrDefault(jObject, "referencedTransactionFullHash", obj => obj.ToString());
+            transaction.PublicKeyAnnouncement = attachmentConverter.GetPublicKeyAnnouncement();
+            transaction.Sender = GetValueOrDefault(jObject, "sender", Convert.ToUInt64);
+            transaction.SenderRs = GetValueOrDefault(jObject, "senderRS", obj => obj.ToString());
+            transaction.SenderPublicKey = GetValueOrDefault(jObject, "senderPublicKey", obj => obj.ToString());
+            transaction.Signature = GetValueOrDefault(jObject, "signature", obj => obj.ToString());
+            transaction.SignatureHash = GetValueOrDefault(jObject, "signatureHash", obj => obj.ToString());
+            transaction.SubType = GetValueOrDefault(jObject, "subType", Convert.ToByte);
+            transaction.Timestamp = GetDateTimeOrDefault(jObject, "timestamp");
+            transaction.TransactionId = GetValueOrNull(jObject, "transaction", Convert.ToUInt64);
+            transaction.TransactionIndex = GetValueOrDefault(jObject, "transactionIndex", Convert.ToInt32);
+            transaction.Type = GetValueOrDefault(jObject, "type", Convert.ToByte);
+            transaction.Version = GetValueOrDefault(jObject, "version", Convert.ToInt32);
+            return transaction;
+        }
+
+        private static T GetValueOrDefault<T>(JToken jToken, string tokenName, Func<object, T> convertFunc)
+        {
+            JValue jValue;
+            return (jValue = jToken.SelectToken(tokenName) as JValue) != null ? convertFunc(jValue.Value) : default(T);
+        }
+
+        private static T? GetValueOrNull<T>(JToken jToken, string tokenName, Func<object, T> convertFunc) where T : struct
+        {
+            JValue jValue;
+            return (jValue = jToken.SelectToken(tokenName) as JValue) != null ? convertFunc(jValue.Value) : (T?) null;
+        }
+
+        private static DateTime GetDateTimeOrDefault(JToken jToken, string tokenName)
+        {
+            var value = GetValueOrNull(jToken, tokenName, Convert.ToInt32);
+            return value.HasValue ? new DateTimeConverter().GetFromNxtTime(value.Value) : new DateTime();
+        }
+
+        private static DateTime? GetDateTimeOrNull(JToken jToken, string tokenName)
+        {
+            var value = GetValueOrNull(jToken, tokenName, Convert.ToInt32);
+            return value.HasValue ? new DateTimeConverter().GetFromNxtTime(value.Value) : (DateTime?) null;
+        }
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
