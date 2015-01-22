033667f9657d25f914dbe27a086a3d4a8da4a576
Liberty Swede <libertyswede@outlook.com>
2015-01-22 19:51:45 +0100

    Added missing file

---
+++ NxtLib/EncryptedDataReply.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using NxtLib.Internal;
+
+namespace NxtLib
+{
+    public class EncryptedDataReply : BaseReply
+    {
+        public string Data { get; set; }
+
+        [JsonConverter(typeof(ByteToHexStringConverter))]
+        public IEnumerable<byte> Nonce { get; set; }
+
+        public IEnumerable<byte> GetDataBytes()
+        {
+            return ByteToHexStringConverter.ToByteArray(Data);
+        }
+    }
+}
