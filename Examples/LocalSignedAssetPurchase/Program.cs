0ae2769848da21daa4bf82e85544b7861147811e
Liberty Swede <libertyswede@outlook.com>
2015-02-17 07:53:48 +0100

    Sample program that uses local signing to make an asset purchase

---
+++ Examples/LocalSignedAssetPurchase/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LocalSignedAssetPurchase
+{
+    class Program
+    {
+        private const string SecretPhrase = "abc123";
+
+        static void Main(string[] args)
+        {
+            // Step 1, generate public key locally
+            var localCrypto = new LocalCrypto();
+            var publicKey = localCrypto.GetPublicKey(SecretPhrase);
+            Console.WriteLine("My public key is: " + publicKey.ToHexString());
+
+            // Step 2, use public key to let a NXT server generate an unsigned transaction
+            var assetExchangeService = new AssetExchangeService();
+            var placeAskOrderReply = assetExchangeService.PlaceAskOrder(123, 1 * 100, Amount.FromNxt(30), new CreateTransactionByPublicKey(1440, Amount.OneNxt, publicKey)).Result;
+
+            // Step 3, sign the transaction locally
+            var json = localCrypto.SignTransaction(placeAskOrderReply, SecretPhrase);
+
+            // Step 4, Broadcast the signed transaction
+            var transactionService = new TransactionService();
+            var broadcastReply = transactionService.BroadcastTransaction(new TransactionParameter(json)).Result;
+            Console.WriteLine("Transaction created, transactionId: " + broadcastReply.TransactionId);
+        }
+    }
+}
