12758f1643fcda259c6f51fc7a62a2b942b59607
Liberty Swede <libertyswede@outlook.com>
2015-02-23 23:07:51 +0100

    Added yet another sample program, dividend tracker, to aid those who have hard time keeping track of dividends here: https://nxtforum.org/assets-board/record-of-invisible-dividends-(sticky-requested)/

---
+++ Examples/DividendTracker/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using NxtLib;
+using NxtLib.AssetExchange;
+using NxtLib.Blocks;
+using NxtLib.ServerInfo;
+using TransactionSubType = NxtLib.TransactionSubType;
+
+namespace DividendTracker
+{
+    /// <summary>
+    /// Simple program that prints out all dividends payed out by using the built in dividend transaction type.
+    /// To help out the folks on nxtforum: https://nxtforum.org/assets-board/record-of-invisible-dividends-(sticky-requested)/
+    /// Start it with command line arguments: -height x to set the blockchein height where it should start scan
+    /// Assumes you have an Nxt node running on localhost on default port (7876) open.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var blockService = new BlockService();
+            var serverInfoService = new ServerInfoService();
+            var assetService = new AssetExchangeService();
+            var currentHeight = serverInfoService.GetBlockchainStatus().Result.NumberOfBlocks;
+            var blockHeight = 335690;
+
+            if (args.Length > 0 && args[0].Equals("-height", StringComparison.InvariantCultureIgnoreCase))
+                Int32.TryParse(args[1], out blockHeight);
+
+            Console.WriteLine("Start scanning blockchain at height: {0}", blockHeight);
+            for (; blockHeight < currentHeight; blockHeight++)
+            {
+                var block = blockService.GetBlockIncludeTransactions(BlockLocator.Height(blockHeight)).Result;
+                foreach (var transaction in block.Transactions.Where(transaction => transaction.SubType == TransactionSubType.ColoredCoinsDividendPayment))
+                {
+                    var attachment = (ColoredCoinsDividendPaymentAttachment)transaction.Attachment;
+                    var asset = assetService.GetAsset(attachment.AssetId).Result;
+                    Console.WriteLine("{0} {1} {2} {3}", transaction.Timestamp.ToString("d"), transaction.TransactionId, asset.Name, attachment.AmountPerQnt.Nxt);
+                }
+            }
+            Console.WriteLine("Last checked block height: {0}", blockHeight);
+            Console.ReadLine();
+        }
+    }
+}
