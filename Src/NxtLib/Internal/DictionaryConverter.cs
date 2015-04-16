befad0f2e7721b7d6d6fa8b14f1b9e21459f3286
Liberty Swede <libertyswede@outlook.com>
2015-04-16 13:59:01 +0200

    Fixed getConstants api reply

---
+++ Src/NxtLib/Internal/DictionaryConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace NxtLib.Internal
+{
+    internal class DictionaryConverter<TKey, TValue> : JsonConverter
+    {
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.StartObject)
+            {
+                var reply = new Dictionary<TKey, TValue>();
+                reader.Read();
+                while (reader.TokenType != JsonToken.EndObject)
+                {
+                    var propertyName = (TKey) reader.Value;
+                    reader.Read();
+                    var propertyValue = (TValue) reader.Value;
+                    reader.Read();
+                    reply.Add(propertyName, propertyValue);
+                }
+                return reply;
+            }
+            throw new ArgumentException();
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Src/NxtLib/ServerInfo/GetConstantsReply.cs
+++ Src/NxtLib/ServerInfo/GetConstantsReply.cs
@@ -6,17 +6,30 @@ namespace NxtLib.ServerInfo
 {
     public class GetConstantsReply : BaseReply
     {
-        public List<object> CurrencyTypes { get; set; }
-        public int MaxArbitraryMessageLength { get; set; }
-        public int MaxBlockPayloadLength { get; set; }
+        [JsonConverter(typeof(DictionaryConverter<string, long>))]
+        public Dictionary<string, long> CurrencyTypes { get; set; }
 
         [JsonConverter(typeof(StringToIntegralTypeConverter))]
         public ulong GenesisAccountId { get; set; }
 
         [JsonConverter(typeof(StringToIntegralTypeConverter))]
         public ulong GenesisBlockId { get; set; }
-        public List<object> HashAlgorithms { get; set; }
-        public List<PeerState> PeerStates { get; set; }
+
+        [JsonConverter(typeof(DictionaryConverter<string, long>))]
+        public Dictionary<string, long> HashAlgorithms { get; set; }
+        public int MaxArbitraryMessageLength { get; set; }
+        public int MaxBlockPayloadLength { get; set; }
+
+        [JsonConverter(typeof(DictionaryConverter<string, long>))]
+        public Dictionary<string, long> MinBalanceModels { get; set; }
+
+        [JsonConverter(typeof(DictionaryConverter<string, long>))]
+        public Dictionary<string, long> PeerStates { get; set; }
+
+        [JsonConverter(typeof(TransactionTypesConverter))]
         public Dictionary<int, Dictionary<int, TransactionType>> TransactionTypes { get; set; }
+
+        [JsonConverter(typeof(DictionaryConverter<string, long>))]
+        public Dictionary<string, long> VotingModels { get; set; }
     }
 }
