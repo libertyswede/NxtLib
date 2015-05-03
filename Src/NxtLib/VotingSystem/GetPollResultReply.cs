fe895f135f75c875ce022ed13c9510d0e74204f6
Liberty Swede <libertyswede@outlook.com>
2015-05-03 19:19:54 +0200

    Added missing properties to GetPollResultReply

--- Src/NxtLib/VotingSystem/GetPollResultReply.cs
+++ Src/NxtLib/VotingSystem/GetPollResultReply.cs
@@ -1,15 +1,26 @@
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using NxtLib.Internal;
 
 namespace NxtLib.VotingSystem
 {
     public class GetPollResultReply : BaseReply
     {
+        public int? Decimals { get; set; }
         public bool Finished { get; set; }
+
+        [JsonProperty(PropertyName = "holding")]
+        [JsonConverter(typeof(StringToIntegralTypeConverter))]
+        public ulong HoldingId { get; set; }
+        public MinBalanceModel MinBalanceModel { get; set; }
+
+        [JsonConverter(typeof(StringToIntegralTypeConverter))]
+        public long MinBalance { get; set; }
         public List<string> Options { get; set; }
 
         [JsonProperty(PropertyName = "poll")]
         public ulong PollId { get; set; }
         public List<PollResult> Results { get; set; }
+        public VotingModel? VotingModel { get; set; }
     }
 }
