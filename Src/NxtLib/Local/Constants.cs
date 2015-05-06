04a78fc3869e26f73a503a56fc533b199b0baaf9
Liberty Swede <libertyswede@outlook.com>
2015-05-06 23:38:57 +0200

    Added constants

---
+++ Src/NxtLib/Local/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace NxtLib.Local
+{
+    public class Constants
+    {
+        public const ulong GenesisAccountId = 1739068987193023818;
+        public const ulong GenesisBlockId = 2680262203532249785;
+    }
+}
