b1e11f33f73110aa505e9558e9212f88f008a168
Liberty Swede <libertyswede@outlook.com>
2015-05-07 19:44:13 +0200

    Added GenesisAccountRs to Constants

--- Src/NxtLib/Local/Constants.cs
+++ Src/NxtLib/Local/Constants.cs
@@ -3,6 +3,7 @@
     public class Constants
     {
         public const ulong GenesisAccountId = 1739068987193023818;
+        public const string GenesisAccountRs = "NXT-MRCC-2YLS-8M54-3CMAJ";
         public const ulong GenesisBlockId = 2680262203532249785;
     }
 }
