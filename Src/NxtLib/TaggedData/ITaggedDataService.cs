e61e5c28dfc1b332c2eb9c8fa095385c5230888f
Liberty Swede <libertyswede@outlook.com>
2015-05-28 00:15:30 +0200

    Added UploadTaggedData to interface (done)

--- Src/NxtLib/TaggedData/ITaggedDataService.cs
+++ Src/NxtLib/TaggedData/ITaggedDataService.cs
@@ -1,6 +1,11 @@
+using System.Threading.Tasks;
+
 namespace NxtLib.TaggedData
 {
     public interface ITaggedDataService
     {
+        Task<TransactionCreatedReply> UploadTaggedData(string name, string data, CreateTransactionParameters parameters,
+            string description = null, string tags = null, string channel = null, string type = null, 
+            bool? isText = null, string filename = null);
     }
 }
