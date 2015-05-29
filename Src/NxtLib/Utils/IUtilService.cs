517da32c3e29324d2bf45d80d83783e319195539
Liberty Swede <libertyswede@outlook.com>
2015-05-29 23:15:18 +0200

    Added hexConvert to interface

--- Src/NxtLib/Utils/IUtilService.cs
+++ Src/NxtLib/Utils/IUtilService.cs
@@ -4,6 +4,7 @@ namespace NxtLib.Utils
 {
     public interface IUtilService
     {
+        Task<HexConvertReply> HexConvert(string @string);
         Task<LongConvertReply> LongConvert(ulong id);
         Task<RsConvertReply> RsConvert(string accountId);
     }
