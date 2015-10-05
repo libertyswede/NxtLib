8e13e7645cc197211c2a4ca5257517ecd1fb16c8
Liberty Swede <libertyswede@outlook.com>
2015-10-05 21:13:36 +0200

    Spacing

--- Src/NxtLib/Forging/IForgingService.cs
+++ Src/NxtLib/Forging/IForgingService.cs
@@ -5,8 +5,11 @@ namespace NxtLib.Forging
     public interface IForgingService
     {
         Task<GetForgingReply> GetForging(SecretPhraseOrAdminPassword secretPhraseOrAdminPassword);
+
         Task<TransactionCreatedReply> LeaseBalance(int period, string recipient, CreateTransactionParameters parameters);
+
         Task<StartForgingReply> StartForging(string secretPhrase);
+
         Task<StopForgingReply> StopForging(SecretPhraseOrAdminPassword secretPhraseOrAdminPassword);
     }
 }
