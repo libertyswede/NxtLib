63f0692597bf6fc2451b3a762f9a7c3980a559d3
Liberty Swede <libertyswede@outlook.com>
2015-10-05 19:19:22 +0200

    Spacing

--- Src/NxtLib/Aliases/IAliasService.cs
+++ Src/NxtLib/Aliases/IAliasService.cs
@@ -6,12 +6,21 @@ namespace NxtLib.Aliases
     public interface IAliasService
     {
         Task<TransactionCreatedReply> BuyAlias(AliasLocator query, Amount amount, CreateTransactionParameters parameters);
+
         Task<TransactionCreatedReply> DeleteAlias(AliasLocator query, CreateTransactionParameters parameters);
+
         Task<AliasReply> GetAlias(AliasLocator query);
+
         Task<AliasCountReply> GetAliasCount(string accoun);
-        Task<AliasesReply> GetAliases(string accountId, DateTime? timeStamp = null, int? firstIndex = null, int? lastIndex = null);
+
+        Task<AliasesReply> GetAliases(string accountId, DateTime? timeStamp = null, int? firstIndex = null,
+            int? lastIndex = null);
+
         Task<AliasesReply> GetAliasesLike(string prefix, int? firstIndex = null, int? lastIndex = null);
-        Task<TransactionCreatedReply> SellAlias(AliasLocator query, Amount price, CreateTransactionParameters parameters, string recipient = null);
+
+        Task<TransactionCreatedReply> SellAlias(AliasLocator query, Amount price, CreateTransactionParameters parameters,
+            string recipient = null);
+
         Task<TransactionCreatedReply> SetAlias(string aliasName, string aliasUri, CreateTransactionParameters parameters);
     }
 }
