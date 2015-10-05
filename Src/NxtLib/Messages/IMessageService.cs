2bb6e14fd26235087fe0e93e6094618bcadaef02
Liberty Swede <libertyswede@outlook.com>
2015-10-05 21:29:16 +0200

    Added requireBlock and requireLastBlock parameters to message api functions

--- Src/NxtLib/Messages/IMessageService.cs
+++ Src/NxtLib/Messages/IMessageService.cs
@@ -6,32 +6,41 @@ namespace NxtLib.Messages
 {
     public interface IMessageService
     {
-        Task<DecryptedReply> DecryptFrom(string accountId, BinaryHexString data, bool messageIsText, 
+        Task<DecryptedReply> DecryptFrom(string accountId, BinaryHexString data, bool decryptedMessageIsText,
             BinaryHexString nonce, bool uncompressDecryptedMessage, string secretPhrase);
 
         Task<DecryptedReply> DecryptFrom(string accountId, EncryptedMessage encryptedMessage, string secretPhrase);
 
-        Task<EncryptedDataReply> EncryptTo(string recipientAccountId, string message, bool compressMessageToEncrypt, string secretPhrase);
+        Task<EncryptedDataReply> EncryptTo(string recipientAccountId, string messageToEncrypt,
+            bool compressMessageToEncrypt, string secretPhrase);
 
-        Task<EncryptedDataReply> EncryptTo(string recipientAccountId, IEnumerable<byte> data, bool compressMessageToEncrypt, string secretPhrase);
+        Task<EncryptedDataReply> EncryptTo(string recipientAccountId, IEnumerable<byte> messageToEncrypt,
+            bool compressMessageToEncrypt, string secretPhrase);
 
         Task<PrunableMessagesReply> GetAllPrunableMessages(int? firstIndex = null, int? lastIndex = null,
-            DateTime? timestamp = null);
+            DateTime? timestamp = null, ulong? requireBlock = null, ulong? requireLastBlock = null);
 
-        Task<PrunableMessageReply> GetPrunableMessage(ulong transactionId, string secretPhrase = null);
+        Task<PrunableMessageReply> GetPrunableMessage(ulong transactionId, string secretPhrase = null,
+            ulong? requireBlock = null, ulong? requireLastBlock = null);
 
         Task<PrunableMessagesReply> GetPrunableMessages(string accountId, string otherAccountId = null,
-            string secretPhrase = null, int? firstIndex = null, int? lastIndex = null, DateTime? timestamp = null);
+            string secretPhrase = null, int? firstIndex = null, int? lastIndex = null, DateTime? timestamp = null,
+            ulong? requireBlock = null, ulong? requireLastBlock = null);
 
-        Task<ReadMessageReply> ReadMessage(ulong transactionId, string secretPhrase = null);
+        Task<ReadMessageReply> ReadMessage(ulong transactionId, string secretPhrase = null, ulong? requireBlock = null,
+            ulong? requireLastBlock = null);
 
         Task<TransactionCreatedReply> SendMessage(CreateTransactionParameters parameters, string recipient = null);
 
-        Task<VerifyPrunableMessageReply> VerifyPrunableMessage(ulong transactionId, string message);
+        Task<VerifyPrunableMessageReply> VerifyPrunableMessage(ulong transactionId, string message,
+            ulong? requireBlock = null, ulong? requireLastBlock = null);
 
-        Task<VerifyPrunableMessageReply> VerifyPrunableMessage(ulong transactionId, IEnumerable<byte> data);
+        Task<VerifyPrunableMessageReply> VerifyPrunableMessage(ulong transactionId, IEnumerable<byte> message,
+            ulong? requireBlock = null, ulong? requireLastBlock = null);
 
-        Task<VerifyPrunableEncryptedMessageReply> VerifyPrunableEncryptedMessage(ulong transactionId, BinaryHexString encryptedMessageData, 
-            BinaryHexString encryptedMessageNonce, bool? messageToEncryptIsText = null, bool? compressMessageToEncrypt = null);
+        Task<VerifyPrunableEncryptedMessageReply> VerifyPrunableEncryptedMessage(ulong transactionId,
+            BinaryHexString encryptedMessageData,
+            BinaryHexString encryptedMessageNonce, bool? messageToEncryptIsText = null,
+            bool? compressMessageToEncrypt = null, ulong? requireBlock = null, ulong? requireLastBlock = null);
     }
 }
--- Src/NxtLib/Messages/MessageService.cs
+++ Src/NxtLib/Messages/MessageService.cs
@@ -13,25 +13,128 @@ namespace NxtLib.Messages
         {
         }
 
-        public MessageService(IDateTimeConverter dateTimeConverter) 
+        public MessageService(IDateTimeConverter dateTimeConverter)
             : base(dateTimeConverter)
         {
         }
 
-        public async Task<DecryptedReply> DecryptFrom(string accountId, BinaryHexString data, bool messageIsText, BinaryHexString nonce,
+        public async Task<DecryptedReply> DecryptFrom(string accountId, BinaryHexString data,
+            bool decryptedMessageIsText, BinaryHexString nonce,
             bool uncompressDecryptedMessage, string secretPhrase)
         {
-            return await DecryptFrom(accountId, data.ToHexString(), messageIsText, nonce.ToHexString(), 
+            return await DecryptFrom(accountId, data.ToHexString(), decryptedMessageIsText, nonce.ToHexString(),
                 uncompressDecryptedMessage, secretPhrase);
         }
-        
-        public async Task<DecryptedReply> DecryptFrom(string accountId, EncryptedMessage encryptedMessage, string secretPhrase)
+
+        public async Task<DecryptedReply> DecryptFrom(string accountId, EncryptedMessage encryptedMessage,
+            string secretPhrase)
         {
-            return await DecryptFrom(accountId, encryptedMessage.Data.ToHexString(), encryptedMessage.IsText, 
+            return await DecryptFrom(accountId, encryptedMessage.Data.ToHexString(), encryptedMessage.IsText,
                 encryptedMessage.Nonce.ToHexString(), encryptedMessage.IsCompressed, secretPhrase);
         }
 
-        private async Task<DecryptedReply> DecryptFrom(string accountId, string data, bool decryptedMessageIsText, string nonce,
+        public async Task<EncryptedDataReply> EncryptTo(string recipientAccountId, string messageToEncrypt,
+            bool compressMessageToEncrypt, string secretPhrase)
+        {
+            return await EncryptTo(recipientAccountId, messageToEncrypt, true, compressMessageToEncrypt, secretPhrase);
+        }
+
+        public async Task<EncryptedDataReply> EncryptTo(string recipientAccountId, IEnumerable<byte> messageToEncrypt,
+            bool compressMessageToEncrypt, string secretPhrase)
+        {
+            return await EncryptTo(recipientAccountId, ByteToHexStringConverter.ToHexString(messageToEncrypt), false,
+                compressMessageToEncrypt, secretPhrase);
+        }
+
+        public async Task<PrunableMessagesReply> GetAllPrunableMessages(int? firstIndex = null, int? lastIndex = null,
+            DateTime? timestamp = null, ulong? requireBlock = null, ulong? requireLastBlock = null)
+        {
+            var queryParameters = new Dictionary<string, string>();
+            AddToParametersIfHasValue("firstIndex", firstIndex, queryParameters);
+            AddToParametersIfHasValue("lastIndex", lastIndex, queryParameters);
+            AddToParametersIfHasValue("timestamp", timestamp, queryParameters);
+            AddToParametersIfHasValue("requireBlock", requireBlock, queryParameters);
+            AddToParametersIfHasValue("requireLastBlock", requireLastBlock, queryParameters);
+            return await Get<PrunableMessagesReply>("getAllPrunableMessages", queryParameters);
+        }
+
+        public async Task<PrunableMessageReply> GetPrunableMessage(ulong transactionId, string secretPhrase = null,
+            ulong? requireBlock = null, ulong? requireLastBlock = null)
+        {
+            var queryParameters = new Dictionary<string, string> {{"transaction", transactionId.ToString()}};
+            AddToParametersIfHasValue("secretPhrase", secretPhrase, queryParameters);
+            AddToParametersIfHasValue("requireBlock", requireBlock, queryParameters);
+            AddToParametersIfHasValue("requireLastBlock", requireLastBlock, queryParameters);
+            return await Get<PrunableMessageReply>("getPrunableMessage", queryParameters);
+        }
+
+        public async Task<PrunableMessagesReply> GetPrunableMessages(string accountId, string otherAccountId = null,
+            string secretPhrase = null, int? firstIndex = null, int? lastIndex = null, DateTime? timestamp = null,
+            ulong? requireBlock = null, ulong? requireLastBlock = null)
+        {
+            var queryParameters = new Dictionary<string, string> {{"account", accountId}};
+            AddToParametersIfHasValue("otherAccount", otherAccountId, queryParameters);
+            AddToParametersIfHasValue("secretPhrase", secretPhrase, queryParameters);
+            AddToParametersIfHasValue("firstIndex", firstIndex, queryParameters);
+            AddToParametersIfHasValue("lastIndex", lastIndex, queryParameters);
+            AddToParametersIfHasValue("timestamp", timestamp, queryParameters);
+            AddToParametersIfHasValue("requireBlock", requireBlock, queryParameters);
+            AddToParametersIfHasValue("requireLastBlock", requireLastBlock, queryParameters);
+            return await Get<PrunableMessagesReply>("getPrunableMessages", queryParameters);
+        }
+
+        public async Task<ReadMessageReply> ReadMessage(ulong transactionId, string secretPhrase = null,
+            ulong? requireBlock = null, ulong? requireLastBlock = null)
+        {
+            var queryParameters = new Dictionary<string, string> {{"transaction", transactionId.ToString()}};
+            AddToParametersIfHasValue("secretPhrase", secretPhrase, queryParameters);
+            AddToParametersIfHasValue("requireBlock", requireBlock, queryParameters);
+            AddToParametersIfHasValue("requireLastBlock", requireLastBlock, queryParameters);
+            return await Get<ReadMessageReply>("readMessage", queryParameters);
+        }
+
+        public async Task<TransactionCreatedReply> SendMessage(CreateTransactionParameters parameters,
+            string recipient = null)
+        {
+            var queryParameters = new Dictionary<string, string>();
+            AddToParametersIfHasValue("recipient", recipient, queryParameters);
+            parameters.AppendToQueryParameters(queryParameters);
+            return await Post<TransactionCreatedReply>("sendMessage", queryParameters);
+        }
+
+        public async Task<VerifyPrunableMessageReply> VerifyPrunableMessage(ulong transactionId, string message,
+            ulong? requireBlock = null, ulong? requireLastBlock = null)
+        {
+            return await VerifyPrunableMessage(transactionId, message, true, requireBlock, requireLastBlock);
+        }
+
+        public async Task<VerifyPrunableMessageReply> VerifyPrunableMessage(ulong transactionId,
+            IEnumerable<byte> message, ulong? requireBlock = null, ulong? requireLastBlock = null)
+        {
+            return await VerifyPrunableMessage(transactionId, ByteToHexStringConverter.ToHexString(message), 
+                false, requireBlock, requireLastBlock);
+        }
+
+        public async Task<VerifyPrunableEncryptedMessageReply> VerifyPrunableEncryptedMessage(ulong transactionId,
+            BinaryHexString encryptedMessageData, BinaryHexString encryptedMessageNonce,
+            bool? messageToEncryptIsText = null, bool? compressMessageToEncrypt = null, ulong? requireBlock = null,
+            ulong? requireLastBlock = null)
+        {
+            var queryParameters = new Dictionary<string, string>
+            {
+                {"transaction", transactionId.ToString()},
+                {"encryptedMessageData", encryptedMessageData.ToHexString()},
+                {"encryptedMessageNonce", encryptedMessageNonce.ToHexString()}
+            };
+            AddToParametersIfHasValue("messageToEncryptIsText", messageToEncryptIsText, queryParameters);
+            AddToParametersIfHasValue("compressMessageToEncrypt", compressMessageToEncrypt, queryParameters);
+            AddToParametersIfHasValue("requireBlock", requireBlock, queryParameters);
+            AddToParametersIfHasValue("requireLastBlock", requireLastBlock, queryParameters);
+            return await Get<VerifyPrunableEncryptedMessageReply>("verifyPrunableMessage", queryParameters);
+        }
+
+        private async Task<DecryptedReply> DecryptFrom(string accountId, string data, bool decryptedMessageIsText,
+            string nonce,
             bool uncompressDecryptedMessage, string secretPhrase)
         {
             var queryParameters = new Dictionary<string, string>
@@ -46,49 +149,8 @@ namespace NxtLib.Messages
             return await Get<DecryptedReply>("decryptFrom", queryParameters);
         }
 
-        public async Task<EncryptedDataReply> EncryptTo(string recipientAccountId, string message,
-            bool compressMessageToEncrypt, string secretPhrase)
-        {
-            return await EncryptTo(recipientAccountId, message, true, compressMessageToEncrypt, secretPhrase);
-        }
-
-        public async Task<EncryptedDataReply> EncryptTo(string recipientAccountId, IEnumerable<byte> data,
-            bool compressMessageToEncrypt, string secretPhrase)
-        {
-            return await EncryptTo(recipientAccountId, ByteToHexStringConverter.ToHexString(data), false,
-                compressMessageToEncrypt, secretPhrase);
-        }
-
-        public async Task<PrunableMessagesReply> GetAllPrunableMessages(int? firstIndex = null, int? lastIndex = null,
-            DateTime? timestamp = null)
-        {
-            var queryParameters = new Dictionary<string, string>();
-            AddToParametersIfHasValue("firstIndex", firstIndex, queryParameters);
-            AddToParametersIfHasValue("lastIndex", lastIndex, queryParameters);
-            AddToParametersIfHasValue("timestamp", timestamp, queryParameters);
-            return await Get<PrunableMessagesReply>("getAllPrunableMessages", queryParameters);
-        }
-
-        public async Task<PrunableMessageReply> GetPrunableMessage(ulong transactionId, string secretPhrase = null)
-        {
-            var queryParameters = new Dictionary<string, string> {{"transaction", transactionId.ToString()}};
-            AddToParametersIfHasValue("secretPhrase", secretPhrase, queryParameters);
-            return await Get<PrunableMessageReply>("getPrunableMessage", queryParameters);
-        }
-
-        public async Task<PrunableMessagesReply> GetPrunableMessages(string accountId, string otherAccountId = null, string secretPhrase = null,
-            int? firstIndex = null, int? lastIndex = null, DateTime? timestamp = null)
-        {
-            var queryParameters = new Dictionary<string, string> {{"account", accountId}};
-            AddToParametersIfHasValue("otherAccount", otherAccountId, queryParameters);
-            AddToParametersIfHasValue("secretPhrase", secretPhrase, queryParameters);
-            AddToParametersIfHasValue("firstIndex", firstIndex, queryParameters);
-            AddToParametersIfHasValue("lastIndex", lastIndex, queryParameters);
-            AddToParametersIfHasValue("timestamp", timestamp, queryParameters);
-            return await Get<PrunableMessagesReply>("getPrunableMessages", queryParameters);
-        }
-
-        private async Task<EncryptedDataReply> EncryptTo(string recipientAccountId, string messageToEncrypt, bool messageToEncryptIsText,
+        private async Task<EncryptedDataReply> EncryptTo(string recipientAccountId, string messageToEncrypt,
+            bool messageToEncryptIsText,
             bool compressMessageToEncrypt, string secretPhrase)
         {
             var queryParameters = new Dictionary<string, string>
@@ -102,32 +164,8 @@ namespace NxtLib.Messages
             return await Get<EncryptedDataReply>("encryptTo", queryParameters);
         }
 
-        public async Task<ReadMessageReply> ReadMessage(ulong transactionId, string secretPhrase = null)
-        {
-            var queryParameters = new Dictionary<string, string> {{"transaction", transactionId.ToString()}};
-            AddToParametersIfHasValue("secretPhrase", secretPhrase, queryParameters);
-            return await Get<ReadMessageReply>("readMessage", queryParameters);
-        }
-
-        public async Task<TransactionCreatedReply> SendMessage(CreateTransactionParameters parameters, string recipient = null)
-        {
-            var queryParameters = new Dictionary<string, string>();
-            AddToParametersIfHasValue("recipient", recipient, queryParameters);
-            parameters.AppendToQueryParameters(queryParameters);
-            return await Post<TransactionCreatedReply>("sendMessage", queryParameters);
-        }
-
-        public async Task<VerifyPrunableMessageReply> VerifyPrunableMessage(ulong transactionId, string message)
-        {
-            return await VerifyPrunableMessage(transactionId, message, true);
-        }
-
-        public async Task<VerifyPrunableMessageReply> VerifyPrunableMessage(ulong transactionId, IEnumerable<byte> data)
-        {
-            return await VerifyPrunableMessage(transactionId, ByteToHexStringConverter.ToHexString(data), false);
-        }
-
-        private async Task<VerifyPrunableMessageReply> VerifyPrunableMessage(ulong transactionId, string message, bool messageIsText)
+        private async Task<VerifyPrunableMessageReply> VerifyPrunableMessage(ulong transactionId, string message,
+            bool messageIsText, ulong? requireBlock = null, ulong? requireLastBlock = null)
         {
             var queryParameters = new Dictionary<string, string>
             {
@@ -136,22 +174,9 @@ namespace NxtLib.Messages
                 {"messageIsText", messageIsText.ToString()},
                 {"messageIsPrunable", true.ToString()}
             };
+            AddToParametersIfHasValue("requireBlock", requireBlock, queryParameters);
+            AddToParametersIfHasValue("requireLastBlock", requireLastBlock, queryParameters);
             return await Get<VerifyPrunableMessageReply>("verifyPrunableMessage", queryParameters);
-        }
-
-        public async Task<VerifyPrunableEncryptedMessageReply> VerifyPrunableEncryptedMessage(ulong transactionId, BinaryHexString encryptedMessageData, 
-            BinaryHexString encryptedMessageNonce, bool? messageToEncryptIsText = null, bool? compressMessageToEncrypt = null)
-        {
-            var queryParameters = new Dictionary<string, string>
-            {
-                {"transaction", transactionId.ToString()},
-                {"encryptedMessageData", encryptedMessageData.ToHexString()},
-                {"encryptedMessageNonce", encryptedMessageNonce.ToHexString()},
-                {"encryptedMessageIsPrunable", true.ToString()}
-            };
-            AddToParametersIfHasValue("messageToEncryptIsText", messageToEncryptIsText, queryParameters);
-            AddToParametersIfHasValue("compressMessageToEncrypt", compressMessageToEncrypt, queryParameters);
-            return await Get<VerifyPrunableEncryptedMessageReply>("verifyPrunableMessage", queryParameters);
         }
     }
 }
