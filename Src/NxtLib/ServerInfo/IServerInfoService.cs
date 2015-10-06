f0a691182c03dc19c27f09439a3cc2592b4ac5d5
Liberty Swede <libertyswede@outlook.com>
2015-10-06 10:58:43 +0200

    Spacing

--- Src/NxtLib/ServerInfo/IServerInfoService.cs
+++ Src/NxtLib/ServerInfo/IServerInfoService.cs
@@ -5,11 +5,17 @@ namespace NxtLib.ServerInfo
     public interface IServerInfoService
     {
         Task<EventRegisterReply> EventRegister(NxtEvent? nxtEvent = null, bool? add = null, bool? remove = null);
+
         Task<EventWaitReply> EventWait(long timeout);
+
         Task<GetBlockchainStatusReply> GetBlockchainStatus();
+
         Task<GetConstantsReply> GetConstants();
+
         Task<GetPluginsReply> GetPlugins();
+
         Task<GetStateReply> GetState(bool? includeCounts = null, string adminPassword = null);
+
         Task<GetTimeReply> GetTime();
     }
 }
