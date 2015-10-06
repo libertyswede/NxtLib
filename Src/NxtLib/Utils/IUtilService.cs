9bdaecb7aa41162fbc2ca1eaebf11d59a4d6b0da
Liberty Swede <libertyswede@outlook.com>
2015-10-06 12:42:01 +0200

    Minor cleanup

--- Src/NxtLib/Utils/IUtilService.cs
+++ Src/NxtLib/Utils/IUtilService.cs
@@ -5,11 +5,17 @@ namespace NxtLib.Utils
     public interface IUtilService
     {
         Task<DecodeQrCodeReply> DecodeQrCode(string qrCodeBase64);
+
         Task<EncodeQrCodeReply> EncodeQrCode(string qrCodeData, int? width = null, int? height = null);
+
         Task<FullHashToIdReply> FullHashToId(string fullHash);
+
         Task<HashReply> Hash(HashAlgorithm hashAlgorithm, BinaryHexString secret, bool? secretIsText = null);
+
         Task<HexConvertReply> HexConvert(string @string);
+
         Task<LongConvertReply> LongConvert(ulong id);
+
         Task<RsConvertReply> RsConvert(string accountId);
     }
 }
--- Src/NxtLib/Utils/UtilService.cs
+++ Src/NxtLib/Utils/UtilService.cs
@@ -12,7 +12,7 @@ namespace NxtLib.Utils
         {
         }
 
-        public UtilService(IDateTimeConverter dateTimeConverter) 
+        public UtilService(IDateTimeConverter dateTimeConverter)
             : base(dateTimeConverter)
         {
         }
