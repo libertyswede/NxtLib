260c1672c9728008d96012a83fc5cdfa461ac17a
Liberty Swede <libertyswede@outlook.com>
2015-10-06 12:49:20 +0200

    Added requireBlock and requireLastBlock parameters to Voting api functions

--- Src/NxtLib/VotingSystem/IVotingSystemService.cs
+++ Src/NxtLib/VotingSystem/IVotingSystemService.cs
@@ -6,21 +6,29 @@ namespace NxtLib.VotingSystem
 {
     public interface IVotingSystemService
     {
-        Task<TransactionCreatedReply> CastVote(ulong pollId, Dictionary<int, int> votes, CreateTransactionParameters parameters);
+        Task<TransactionCreatedReply> CastVote(ulong pollId, Dictionary<int, int> votes,
+            CreateTransactionParameters parameters);
 
-        Task<TransactionCreatedReply> CreatePoll(CreatePollParameters createPollParameters, CreateTransactionParameters parameters);
+        Task<TransactionCreatedReply> CreatePoll(CreatePollParameters createPollParameters,
+            CreateTransactionParameters parameters);
 
-        Task<GetPollReply> GetPoll(ulong pollId);
+        Task<GetPollReply> GetPoll(ulong pollId, ulong? requireBlock = null, ulong? requireLastBlock = null);
 
         Task<GetPollResultReply> GetPollResult(ulong pollId, VotingModel? votingModel = null,
-            ulong? holdingId = null, long? minBalance = null, MinBalanceModel? minBalanceModel = null);
+            ulong? holdingId = null, long? minBalance = null, MinBalanceModel? minBalanceModel = null,
+            ulong? requireBlock = null, ulong? requireLastBlock = null);
 
         Task<GetPollsReply> GetPolls(string accountId = null, int? firstIndex = null, int? lastIndex = null,
-            DateTime? timestamp = null, bool? includeFinished = null);
-        Task<GetPollVoteReply> GetPollVote(ulong pollId, string accountId);
-        Task<GetPollVotesReply> GetPollVotes(ulong pollId, int? firstIndex = null, int? lastIndex = null);
+            DateTime? timestamp = null, bool? includeFinished = null, ulong? requireBlock = null,
+            ulong? requireLastBlock = null);
+
+        Task<GetPollVoteReply> GetPollVote(ulong pollId, string accountId, bool? includeWeights = null,
+            ulong? requireBlock = null, ulong? requireLastBlock = null);
+
+        Task<GetPollVotesReply> GetPollVotes(ulong pollId, int? firstIndex = null, int? lastIndex = null,
+            bool? includeWeights = null, ulong? requireBlock = null, ulong? requireLastBlock = null);
 
         Task<GetPollsReply> SearchPolls(string query, int? firstIndex = null, int? lastIndex = null,
-            bool? includeFinished = null);
+            bool? includeFinished = null, ulong? requireBlock = null, ulong? requireLastBlock = null);
     }
 }
--- Src/NxtLib/VotingSystem/VotingSystemService.cs
+++ Src/NxtLib/VotingSystem/VotingSystemService.cs
@@ -18,7 +18,8 @@ namespace NxtLib.VotingSystem
         {
         }
 
-        public async Task<TransactionCreatedReply> CastVote(ulong pollId, Dictionary<int, int> votes, CreateTransactionParameters parameters)
+        public async Task<TransactionCreatedReply> CastVote(ulong pollId, Dictionary<int, int> votes,
+            CreateTransactionParameters parameters)
         {
             var queryParameters = new Dictionary<string, string> {{"poll", pollId.ToString()}};
             foreach (var vote in votes)
@@ -29,7 +30,8 @@ namespace NxtLib.VotingSystem
             return await Post<TransactionCreatedReply>("castVote", queryParameters);
         }
 
-        public async Task<TransactionCreatedReply> CreatePoll(CreatePollParameters createPollParameters, CreateTransactionParameters parameters)
+        public async Task<TransactionCreatedReply> CreatePoll(CreatePollParameters createPollParameters,
+            CreateTransactionParameters parameters)
         {
             var queryParameters = new Dictionary<string, string>
             {
@@ -55,6 +57,77 @@ namespace NxtLib.VotingSystem
             return await Post<TransactionCreatedReply>("createPoll", queryParameters);
         }
 
+        public async Task<GetPollReply> GetPoll(ulong pollId, ulong? requireBlock = null, ulong? requireLastBlock = null)
+        {
+            var queryParameters = new Dictionary<string, string> {{"poll", pollId.ToString()}};
+            AddToParametersIfHasValue("requireBlock", requireBlock, queryParameters);
+            AddToParametersIfHasValue("requireLastBlock", requireLastBlock, queryParameters);
+            return await Get<GetPollReply>("getPoll", queryParameters);
+        }
+
+        public async Task<GetPollResultReply> GetPollResult(ulong pollId, VotingModel? votingModel = null,
+            ulong? holdingId = null, long? minBalance = null, MinBalanceModel? minBalanceModel = null,
+            ulong? requireBlock = null, ulong? requireLastBlock = null)
+        {
+            var queryParameters = new Dictionary<string, string> {{"poll", pollId.ToString()}};
+            AddToParametersIfHasValue("votingModel", votingModel.HasValue ? (int?) votingModel.Value : null, queryParameters);
+            AddToParametersIfHasValue("holding", holdingId, queryParameters);
+            AddToParametersIfHasValue("minBalance", minBalance, queryParameters);
+            AddToParametersIfHasValue("minBalanceModel", minBalanceModel.HasValue ? (int?) minBalanceModel.Value : null, queryParameters);
+            AddToParametersIfHasValue("requireBlock", requireBlock, queryParameters);
+            AddToParametersIfHasValue("requireLastBlock", requireLastBlock, queryParameters);
+            return await Get<GetPollResultReply>("getPollResult", queryParameters);
+        }
+
+        public async Task<GetPollsReply> GetPolls(string accountId = null, int? firstIndex = null, int? lastIndex = null,
+            DateTime? timestamp = null, bool? includeFinished = null, ulong? requireBlock = null,
+            ulong? requireLastBlock = null)
+        {
+            var queryParameters = new Dictionary<string, string>();
+            AddToParametersIfHasValue("account", accountId, queryParameters);
+            AddToParametersIfHasValue("firstIndex", firstIndex, queryParameters);
+            AddToParametersIfHasValue("lastIndex", lastIndex, queryParameters);
+            AddToParametersIfHasValue("timestamp", timestamp, queryParameters);
+            AddToParametersIfHasValue("includeFinished", includeFinished, queryParameters);
+            AddToParametersIfHasValue("requireBlock", requireBlock, queryParameters);
+            AddToParametersIfHasValue("requireLastBlock", requireLastBlock, queryParameters);
+            return await Get<GetPollsReply>("getPolls", queryParameters);
+        }
+
+        public async Task<GetPollVoteReply> GetPollVote(ulong pollId, string accountId, bool? includeWeights = null,
+            ulong? requireBlock = null, ulong? requireLastBlock = null)
+        {
+            var queryParameters = new Dictionary<string, string> {{"poll", pollId.ToString()}, {"account", accountId}};
+            AddToParametersIfHasValue("includeWeights", includeWeights, queryParameters);
+            AddToParametersIfHasValue("requireBlock", requireBlock, queryParameters);
+            AddToParametersIfHasValue("requireLastBlock", requireLastBlock, queryParameters);
+            return await Get<GetPollVoteReply>("getPollVote", queryParameters);
+        }
+
+        public async Task<GetPollVotesReply> GetPollVotes(ulong pollId, int? firstIndex = null, int? lastIndex = null,
+            bool? includeWeights = null, ulong? requireBlock = null, ulong? requireLastBlock = null)
+        {
+            var queryParameters = new Dictionary<string, string> {{"poll", pollId.ToString()}};
+            AddToParametersIfHasValue("firstIndex", firstIndex, queryParameters);
+            AddToParametersIfHasValue("lastIndex", lastIndex, queryParameters);
+            AddToParametersIfHasValue("includeWeights", includeWeights, queryParameters);
+            AddToParametersIfHasValue("requireBlock", requireBlock, queryParameters);
+            AddToParametersIfHasValue("requireLastBlock", requireLastBlock, queryParameters);
+            return await Get<GetPollVotesReply>("getPollVotes", queryParameters);
+        }
+
+        public async Task<GetPollsReply> SearchPolls(string query, int? firstIndex = null, int? lastIndex = null,
+            bool? includeFinished = null, ulong? requireBlock = null, ulong? requireLastBlock = null)
+        {
+            var queryParameters = new Dictionary<string, string> {{"query", query}};
+            AddToParametersIfHasValue("firstIndex", firstIndex, queryParameters);
+            AddToParametersIfHasValue("lastIndex", lastIndex, queryParameters);
+            AddToParametersIfHasValue("includeFinished", includeFinished, queryParameters);
+            AddToParametersIfHasValue("requireBlock", requireBlock, queryParameters);
+            AddToParametersIfHasValue("requireLastBlock", requireLastBlock, queryParameters);
+            return await Get<GetPollsReply>("searchPolls", queryParameters);
+        }
+
         public async Task<TransactionCreatedReply> CreatePoll(string name, string description, int finishHeight,
             VotingModel votingModel, int minNumberOfOptions, int maxNumberOfOptions, int minRangeValue,
             int maxRangeValue, List<string> options, CreateTransactionParameters parameters, long? minBalance = null,
@@ -76,66 +149,10 @@ namespace NxtLib.VotingSystem
                 queryParameters.Add("option" + i.ToString().PadLeft(2, '0'), options[i]);
             }
             AddToParametersIfHasValue("minBalance", minBalance, queryParameters);
-            AddToParametersIfHasValue("minBalanceModel", minBalanceModel.HasValue ? (int?) minBalanceModel.Value : null,
-                queryParameters);
+            AddToParametersIfHasValue("minBalanceModel", minBalanceModel.HasValue ? (int?) minBalanceModel.Value : null, queryParameters);
             AddToParametersIfHasValue("holding", holdingId, queryParameters);
             parameters.AppendToQueryParameters(queryParameters);
             return await Post<TransactionCreatedReply>("createPoll", queryParameters);
-        }
-
-        public async Task<GetPollReply> GetPoll(ulong pollId)
-        {
-            var queryParameters = new Dictionary<string, string> {{"poll", pollId.ToString()}};
-            return await Get<GetPollReply>("getPoll", queryParameters);
-        }
-
-        public async Task<GetPollResultReply> GetPollResult(ulong pollId, VotingModel? votingModel = null,
-            ulong? holdingId = null, long? minBalance = null, MinBalanceModel? minBalanceModel = null)
-        {
-            var queryParameters = new Dictionary<string, string> {{"poll", pollId.ToString()}};
-            AddToParametersIfHasValue("votingModel", votingModel.HasValue ? (int?)votingModel.Value : null,
-                queryParameters);
-            AddToParametersIfHasValue("holding", holdingId, queryParameters);
-            AddToParametersIfHasValue("minBalance", minBalance, queryParameters);
-            AddToParametersIfHasValue("minBalanceModel", minBalanceModel.HasValue ? (int?)minBalanceModel.Value : null,
-                queryParameters);
-            return await Get<GetPollResultReply>("getPollResult", queryParameters);
-        }
-
-        public async Task<GetPollsReply> GetPolls(string accountId = null, int? firstIndex = null, int? lastIndex = null,
-            DateTime? timestamp = null, bool? includeFinished = null)
-        {
-            var queryParameters = new Dictionary<string, string>();
-            AddToParametersIfHasValue("account", accountId, queryParameters);
-            AddToParametersIfHasValue("firstIndex", firstIndex, queryParameters);
-            AddToParametersIfHasValue("lastIndex", lastIndex, queryParameters);
-            AddToParametersIfHasValue("timestamp", timestamp, queryParameters);
-            AddToParametersIfHasValue("includeFinished", includeFinished, queryParameters);
-            return await Get<GetPollsReply>("getPolls", queryParameters);
-        }
-
-        public async Task<GetPollVoteReply> GetPollVote(ulong pollId, string accountId)
-        {
-            var queryParameters = new Dictionary<string, string> {{"poll", pollId.ToString()}, {"account", accountId}};
-            return await Get<GetPollVoteReply>("getPollVote", queryParameters);
-        }
-
-        public async Task<GetPollVotesReply> GetPollVotes(ulong pollId, int? firstIndex = null, int? lastIndex = null)
-        {
-            var queryParameters = new Dictionary<string, string> {{"poll", pollId.ToString()}};
-            AddToParametersIfHasValue("firstIndex", firstIndex, queryParameters);
-            AddToParametersIfHasValue("lastIndex", lastIndex, queryParameters);
-            return await Get<GetPollVotesReply>("getPollVotes", queryParameters);
-        }
-
-        public async Task<GetPollsReply> SearchPolls(string query, int? firstIndex = null, int? lastIndex = null,
-            bool? includeFinished = null)
-        {
-            var queryParameters = new Dictionary<string, string> {{"query", query}};
-            AddToParametersIfHasValue("firstIndex", firstIndex, queryParameters);
-            AddToParametersIfHasValue("lastIndex", lastIndex, queryParameters);
-            AddToParametersIfHasValue("includeFinished", includeFinished, queryParameters);
-            return await Get<GetPollsReply>("searchPolls", queryParameters);
         }
     }
 }
