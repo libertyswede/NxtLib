9d686ad2c59738da48592217d5bc2b21a9255b2a
Liberty Swede <libertyswede@outlook.com>
2015-10-06 11:27:44 +0200

    Code cleanup

--- Src/NxtLib/Tokens/ITokenService.cs
+++ Src/NxtLib/Tokens/ITokenService.cs
@@ -1,5 +1,4 @@
 using System;
-using System.IO;
 using System.Threading.Tasks;
 
 namespace NxtLib.Tokens
@@ -7,10 +6,15 @@ namespace NxtLib.Tokens
     public interface ITokenService
     {
         Task<TokenReply> DecodeFileToken(string file, string token);
+
         Task<DecodeHallmarkReply> DecodeHallmark(string hallmark);
+
         Task<TokenReply> DecodeToken(string website, string token);
+
         Task<GenerateTokenReply> GenerateFileToken(string secretPhrase, string file);
+
         Task<GenerateTokenReply> GenerateToken(string secretPhrase, string website);
+
         Task<MarkHostReply> MarkHost(string secretPhrase, string host, int weight, DateTime date);
     }
 }
--- Src/NxtLib/Tokens/TokenService.cs
+++ Src/NxtLib/Tokens/TokenService.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Threading.Tasks;
 using NxtLib.Internal;
 using NxtLib.Local;
