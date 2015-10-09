17db704d711884e1b90cb21f684aa829cf6cc618
Liberty Swede <libertyswede@outlook.com>
2015-10-09 15:05:02 +0200

    Bugfix

--- Examples/Dividends/DividendPayoutListing.cs
+++ Examples/Dividends/DividendPayoutListing.cs
@@ -75,7 +75,7 @@ namespace Dividends
             var index = 0;
             while (index < _asset.NumberOfTrades)
             {
-                var getTradesResult = _assetService.GetTrades(AssetIdOrAccountId.ByAssetId(_asset.AssetId), index, index + 100, includeAssetInfo: false).Result;
+                var getTradesResult = _assetService.GetTrades(AssetIdOrAccountId.ByAssetId(_asset.AssetId), index, index + 99, includeAssetInfo: false).Result;
                 foreach (var trade in getTradesResult.Trades.Where(t => t.Height <= height))
                 {
                     UpdateOwnership(owners, trade.BuyerRs, trade.SellerRs, trade.QuantityQnt);
@@ -85,7 +85,7 @@ namespace Dividends
             index = 0;
             while (index < _asset.NumberOfTransfers)
             {
-                var getTransfersResult = _assetService.GetAssetTransfers(AssetIdOrAccountId.ByAssetId(_asset.AssetId), index, index + 100, false).Result;
+                var getTransfersResult = _assetService.GetAssetTransfers(AssetIdOrAccountId.ByAssetId(_asset.AssetId), index, index + 99, false).Result;
                 foreach (var transfer in getTransfersResult.Transfers.Where(t => t.Height <= height))
                 {
                     UpdateOwnership(owners, transfer.RecipientRs, transfer.SenderRs, transfer.QuantityQnt);
