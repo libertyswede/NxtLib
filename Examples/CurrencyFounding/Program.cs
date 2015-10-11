8300ff9adcfe356533540edf84278da94f2dac90
Liberty Swede <libertyswede@outlook.com>
2015-10-11 22:28:38 +0200

    Added sample project CurrencyFounding

---
+++ Examples/CurrencyFounding/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using NxtLib.MonetarySystem;
+
+namespace CurrencyFounding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new MonetarySystemService();
+            var currency = service.GetCurrency(CurrencyLocator.ByCurrencyId(7851677590592900934)).Result;
+            var founders = service.GetCurrencyFounders(7851677590592900934).Result;
+
+            var goal = currency.ReserveSupply * currency.MinReservePerUnit.Nxt;
+
+            Console.WriteLine($"Name:         {currency.Name}");
+            Console.WriteLine($"Code:         {currency.Code}");
+            Console.WriteLine($"Id:           {currency.CurrencyId}");
+            Console.WriteLine($"Reserve Goal: {goal:0,0.0}");
+            Console.WriteLine("---------------------------------------------------");
+
+            foreach (var founder in founders.Founders.OrderByDescending(f => f.AmountPerUnit.Nqt))
+            {
+                var reserved = founder.AmountPerUnit.Nxt * currency.ReserveSupply;
+                Console.WriteLine($"Account:  {founder.AccountRs} - {reserved:0,0.0} NXT");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Examples/CurrencyFounding/run.bat
@@ -0,0 +1,1 @@
+bin\Debug\CurrencyFounding.exe > output.txt
