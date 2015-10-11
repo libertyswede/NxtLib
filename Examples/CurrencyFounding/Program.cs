eb8223d268115b98f5d6feb2b2702f3a9c86fa1a
Liberty Swede <libertyswede@outlook.com>
2015-10-11 22:38:51 +0200

    Minor updates

--- Examples/CurrencyFounding/Program.cs
+++ Examples/CurrencyFounding/Program.cs
@@ -13,11 +13,13 @@ namespace CurrencyFounding
             var founders = service.GetCurrencyFounders(7851677590592900934).Result;
 
             var goal = currency.ReserveSupply * currency.MinReservePerUnit.Nxt;
+            var reservedTotal = founders.Founders.Sum(f => f.AmountPerUnit.Nxt)*currency.ReserveSupply;
 
             Console.WriteLine($"Name:         {currency.Name}");
             Console.WriteLine($"Code:         {currency.Code}");
             Console.WriteLine($"Id:           {currency.CurrencyId}");
-            Console.WriteLine($"Reserve Goal: {goal:0,0.0}");
+            Console.WriteLine($"Reserve Goal: {goal:0,0.0} ({reservedTotal / goal:P})");
+            Console.WriteLine($"Founders:     {founders.Founders.Count}");
             Console.WriteLine("---------------------------------------------------");
 
             foreach (var founder in founders.Founders.OrderByDescending(f => f.AmountPerUnit.Nqt))
