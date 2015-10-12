45be0c41cb84538d1cd69c5372c79a51c8420b66
Liberty Swede <libertyswede@outlook.com>
2015-10-12 09:00:44 +0200

    Hard coded format

--- Examples/CurrencyFounding/Program.cs
+++ Examples/CurrencyFounding/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Threading;
 using NxtLib.MonetarySystem;
 
 namespace CurrencyFounding
@@ -14,18 +15,24 @@ namespace CurrencyFounding
 
             var goal = currency.ReserveSupply * currency.MinReservePerUnit.Nxt;
             var reservedTotal = founders.Founders.Sum(f => f.AmountPerUnit.Nxt)*currency.ReserveSupply;
-
+            
+            var nfi = new System.Globalization.NumberFormatInfo
+            {
+                NumberDecimalSeparator = ".",
+                CurrencyGroupSeparator = ","
+            };
+            
             Console.WriteLine($"Name:         {currency.Name}");
             Console.WriteLine($"Code:         {currency.Code}");
             Console.WriteLine($"Id:           {currency.CurrencyId}");
-            Console.WriteLine($"Reserve Goal: {goal:0,0.0} ({reservedTotal / goal:P})");
+            Console.WriteLine(string.Format(nfi, "Reserve Goal: {0:0,0.0} ({1:P})", goal, reservedTotal/goal));
             Console.WriteLine($"Founders:     {founders.Founders.Count}");
             Console.WriteLine("---------------------------------------------------");
 
             foreach (var founder in founders.Founders.OrderByDescending(f => f.AmountPerUnit.Nqt))
             {
                 var reserved = founder.AmountPerUnit.Nxt * currency.ReserveSupply;
-                Console.WriteLine($"Account:  {founder.AccountRs} - {reserved:0,0.0} NXT");
+                Console.WriteLine(string.Format(nfi, "Account: {0} - {1:0,0.0} NXT", founder.AccountRs, reserved));
             }
             Console.ReadLine();
         }
