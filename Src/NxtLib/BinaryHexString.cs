c69cf324aec0cdc6076212d457e8fc3842d0e461
Liberty Swede <libertyswede@outlook.com>
2015-10-25 16:02:26 +0100

    BinaryHexString now implements IEquatable

--- Src/NxtLib/BinaryHexString.cs
+++ Src/NxtLib/BinaryHexString.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using NxtLib.Internal;
 
 namespace NxtLib
 {
-    public class BinaryHexString
+    public class BinaryHexString : IEquatable<BinaryHexString>
     {
         private readonly string _hexData;
 
@@ -25,6 +26,22 @@ namespace NxtLib
         public IEnumerable<byte> ToBytes()
         {
             return ByteToHexStringConverter.ToBytesFromHexString(_hexData);
+        }
+
+        public bool Equals(BinaryHexString other)
+        {
+            return other != null && other.ToHexString().Equals(ToHexString());
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as BinaryHexString;
+            return other != null && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return ToHexString().GetHashCode();
         }
 
         public override string ToString()
