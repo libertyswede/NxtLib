831d5115788d0c640dbee9ae76de20f7b28f6aec
Liberty Swede <libertyswede@outlook.com>
2015-10-30 00:07:24 +0100

    First stumbling steps toward local message encryption

--- Src/NxtLib/Local/LocalCrypto.cs
+++ Src/NxtLib/Local/LocalCrypto.cs
@@ -1,4 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using Newtonsoft.Json.Linq;
 using NxtLib.Internal;
 using NxtLib.Internal.LocalSign;
@@ -39,6 +43,53 @@ namespace NxtLib.Local
             var attachment = JObject.Parse(transactionCreatedReply.RawJsonReply).SelectToken($"{Parameters.TransactionJson}['{Parameters.Attachment}']");
             var signature = new BinaryHexString(_crypto.Sign(unsignedBytes, secretPhrase));
             return BuildSignedTransaction(transaction, referencedTransactionFullHash, signature, attachment);
+        }
+
+        public byte[] CreateNonce()
+        {
+            var nonce = new byte[32];
+            var random = RandomNumberGenerator.Create();
+            random.GetBytes(nonce);
+            return nonce;
+        }
+
+        public BinaryHexString EncryptTo(string secretPhrase, BinaryHexString recipientPublicKey, string message, byte[] nonce)
+        {
+            var messageBytes = Encoding.UTF8.GetBytes(message);
+            var recipientPublicKeyBytes = recipientPublicKey.ToBytes().ToArray();
+
+            var sha256 = SHA256.Create();
+            var senderSecretBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(secretPhrase));
+            Curve25519.Clamp(senderSecretBytes);
+
+            var dhSharedSecret = new byte[32];
+            Curve25519.Curve(dhSharedSecret, senderSecretBytes, recipientPublicKeyBytes);
+            for (var i = 0; i < 32; i++)
+            {
+                dhSharedSecret[i] ^= nonce[i];
+            }
+            var key = sha256.ComputeHash(dhSharedSecret);
+
+            using (var ms = new MemoryStream())
+            using (var cryptor = Aes.Create())
+            {
+                cryptor.Mode = CipherMode.CBC;
+                cryptor.Padding = PaddingMode.PKCS7;
+                cryptor.KeySize = 128;
+                cryptor.BlockSize = 128;
+
+                var iv = cryptor.IV;
+
+                using (var cs = new CryptoStream(ms, cryptor.CreateEncryptor(key, iv), CryptoStreamMode.Write))
+                {
+                    cs.Write(messageBytes, 0, messageBytes.Length);
+                }
+                var encryptedContent = ms.ToArray();
+                var result = new byte[iv.Length + encryptedContent.Length];
+                Buffer.BlockCopy(iv, 0, result, 0, iv.Length);
+                Buffer.BlockCopy(encryptedContent, 0, result, iv.Length, encryptedContent.Length);
+                return result;
+            }
         }
 
         private static JObject BuildSignedTransaction(Transaction transaction, string referencedTransactionFullHash,
