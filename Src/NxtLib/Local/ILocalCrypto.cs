546f799b888703e504aadc9055b466899e4d72df
Liberty Swede <libertyswede@outlook.com>
2015-11-03 16:26:51 +0100

    Minor

--- Src/NxtLib/Local/ILocalCrypto.cs
+++ Src/NxtLib/Local/ILocalCrypto.cs
@@ -5,13 +5,9 @@ namespace NxtLib.Local
     public interface ILocalCrypto
     {
         BinaryHexString GetPublicKey(string secretPhrase);
-
         JObject SignTransaction(TransactionCreatedReply transactionCreatedReply, string secretPhrase);
-
         ulong GetAccountIdFromPublicKey(BinaryHexString publicKey);
-
         string GetReedSolomonFromAccountId(ulong accountId);
-
         ulong GetAccountIdFromReedSolomon(string reedSolomonAddress);
         BinaryHexString EncryptTextTo(BinaryHexString recipientPublicKey, string message, byte[] nonce, bool compress, string secretPhrase);
         BinaryHexString EncryptDataTo(BinaryHexString recipientPublicKey, BinaryHexString data, byte[] nonce, bool compress, string secretPhrase);
