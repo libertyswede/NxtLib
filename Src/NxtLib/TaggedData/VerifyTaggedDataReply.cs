4ff96aaa4358185a0d876933fb36b1b19395bb99
Liberty Swede <libertyswede@outlook.com>
2015-11-04 13:32:28 +0100

    Updated VerifyTaggedDataReply

--- Src/NxtLib/TaggedData/VerifyTaggedDataReply.cs
+++ Src/NxtLib/TaggedData/VerifyTaggedDataReply.cs
@@ -1,26 +1,12 @@
-﻿using System.Collections.Generic;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using NxtLib.Internal;
 
 namespace NxtLib.TaggedData
 {
-    public class VerifyTaggedDataReply : ITaggedData, IBaseReply
+    public class VerifyTaggedDataReply : BaseReply
     {
-        public string Channel { get; set; }
-        public string Data { get; set; }
-        public string Description { get; set; }
-        public string Filename { get; set; }
-
         [JsonConverter(typeof(ByteToHexStringConverter))]
         public BinaryHexString Hash { get; set; }
-        public bool IsText { get; set; }
-        public string Name { get; set; }
-        public IEnumerable<KeyValuePair<string, string>> PostData { get; set; }
-        public string RequestUri { get; set; }
-        public int RequestProcessingTime { get; set; }
-        public string RawJsonReply { get; set; }
-        public string Tags { get; set; }
-        public string Type { get; set; }
         public bool Verify { get; set; }
 
         [JsonProperty(Parameters.VersionTaggedDataUpload)]
