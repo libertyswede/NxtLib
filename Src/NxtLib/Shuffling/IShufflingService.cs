f7a3ad2bfacb05eeb733fd950ee204f092c68923
Liberty Swede <libertyswede@outlook.com>
2015-11-30 17:00:06 +0100

    Added getAllShufflings & getAccountShufflings to interface

--- Src/NxtLib/Shuffling/IShufflingService.cs
+++ Src/NxtLib/Shuffling/IShufflingService.cs
@@ -1,6 +1,15 @@
-﻿namespace NxtLib.Shuffling
+﻿using System.Threading.Tasks;
+
+namespace NxtLib.Shuffling
 {
     public interface IShufflingService
     {
+        Task<GetShufflingsReply> GetAccountShufflings(Account account, bool? includeFinished = null,
+            bool? includeHoldingInfo = null, int? firstIndex = null, int? lastIndex = null, ulong? requireBlock = null,
+            ulong? requireLastBlock = null);
+
+        Task<GetShufflingsReply> GetAllShufflings(bool? includeFinished = null,
+            bool? includeHoldingInfo = null, int? firstIndex = null, int? lastIndex = null, ulong? requireBlock = null,
+            ulong? requireLastBlock = null);
     }
 }
