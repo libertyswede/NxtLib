92ade0b8717af60975f9e1ee90ec362ec5adfd99
Liberty Swede <libertyswede@outlook.com>
2015-12-15 20:31:17 +0100

    Currency holding parameters added to HoldingInfo

--- Src/NxtLib/Shuffling/HoldingInfo.cs
+++ Src/NxtLib/Shuffling/HoldingInfo.cs
@@ -1,8 +1,21 @@
+using Newtonsoft.Json;
+using NxtLib.Internal;
+
 namespace NxtLib.Shuffling
 {
     public class HoldingInfo
     {
+        public string Code { get; set; }
         public int Decimals { get; set; }
+        public int? IssuanceHeight { get; set; }
+
+        [JsonConverter(typeof(StringToIntegralTypeConverter))]
+        [JsonProperty(PropertyName = Parameters.IssuerAccount)]
+        public ulong? IssuerAccountId { get; set; }
+        public string IssuerAccountRs { get; set; }
         public string Name { get; set; }
+
+        [JsonConverter(typeof(CurrencyTypeConverter))]
+        public CurrencyType? Type { get; set; }
     }
 }
