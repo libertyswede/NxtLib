3a5efd0d1ead4bf014e18f67822bde52c47e7c9d
Liberty Swede <libertyswede@outlook.com>
2016-02-18 14:43:06 +0100

    Added sample program AccountGenerator

---
+++ Examples/AccountGenerator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NxtLib.Accounts;
+using NxtLib.Local;
+
+namespace AccountGenerator
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var generator = new LocalPasswordGenerator();
+            var accountService = new LocalAccountService();
+
+            Console.Write("Enter desired ending:");
+            var ending = Console.ReadLine();
+
+            var secretPhrase = generator.GeneratePasswordWithAccountEnding(ending);
+            var account = accountService.GetAccount(AccountIdLocator.BySecretPhrase(secretPhrase));
+
+            Console.WriteLine($"Generated NXT Address: {account.AccountRs}");
+            Console.WriteLine($"Secret phrase: {secretPhrase}");
+            Console.WriteLine("Press enter to quit.");
+            Console.ReadLine();
+        }
+    }
+}
