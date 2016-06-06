3d0cc7b2ec0aa9ca6036fe9d6a51aa175db635bb
Liberty Swede <libertyswede@outlook.com>
2016-06-06 23:56:11 +0200

    Example using .NET 4.0

---
+++ Examples/Net40Example/Program.cs
@@ -0,0 +1,16 @@
+﻿using NxtLib.Accounts;
+using System;
+
+namespace NxtLibTest40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var accountService = new AccountService();
+            var account = accountService.GetAccount("NXT-8MVA-XCVR-3JC9-2C7C3").Result;
+            Console.WriteLine("Balance is: " + account.Balance.Nxt + " NXT");
+            Console.ReadLine();
+        }
+    }
+}
