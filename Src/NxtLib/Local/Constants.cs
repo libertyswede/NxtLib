ee9bd528259610c7393ee1e97c40f25b8e4e0fef
Liberty Swede <libertyswede@outlook.com>
2016-06-06 23:56:59 +0200

    Version 1.1.13

--- Src/NxtLib/Local/Constants.cs
+++ Src/NxtLib/Local/Constants.cs
@@ -4,7 +4,7 @@ namespace NxtLib.Local
 {
     public class Constants
     {
-        public const string NxtVersionSupport = "1.8.2";
+        public const string NxtVersionSupport = "1.8.3";
         public static readonly Account GenesisAccount = 1739068987193023818;
         public static readonly DateTime EpochBeginning = new DateTime(2013, 11, 24, 12, 0, 0, DateTimeKind.Utc);
         public const ulong GenesisBlockId = 2680262203532249785;
