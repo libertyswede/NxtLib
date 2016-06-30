ccf3e16df28ed4c5ddb1344e6346b3cbae626668
Liberty Swede <libertyswede@outlook.com>
2016-06-30 11:34:41 +0200

    Made some properties in CreateTransactionPhasing non-nullable and instead have default values

--- Src/NxtLib/CreateTransactionPhasing.cs
+++ Src/NxtLib/CreateTransactionPhasing.cs
@@ -10,10 +10,10 @@ namespace NxtLib
         public int FinishHeight { get; set; }
         public BinaryHexString HashedSecret { get; set; }
         public HashAlgorithm? HashedSecretAlgorithm { get; set; }
-        public ulong? HoldingId { get; set; }
+        public ulong HoldingId { get; set; } = 0;
         public List<BinaryHexString> LinkedFullHash { get; set; }
-        public long? MinBalance { get; set; }
-        public MinBalanceModel? MinBalanceModel { get; set; }
+        public long MinBalance { get; set; } = 0;
+        public MinBalanceModel MinBalanceModel { get; set; } = MinBalanceModel.None;
         public long Quorum { get; set; }
         public VotingModel VotingModel { get; set; }
         public List<Account> WhiteListed { get; set; }
@@ -37,17 +37,17 @@ namespace NxtLib
                 queryParameters.Add(Parameters.PhasingVotingModel, ((int)VotingModel).ToString());
                 queryParameters.Add(Parameters.PhasingQuorum, Quorum.ToString());
 
-                if (MinBalance.HasValue)
+                if (MinBalance > 0)
                 {
                     queryParameters.Add(Parameters.PhasingMinBalance, MinBalance.ToString());
                 }
-                if (HoldingId.HasValue)
+                if (HoldingId > 0)
                 {
                     queryParameters.Add(Parameters.PhasingHolding, HoldingId.ToString());
                 }
-                if (MinBalanceModel.HasValue)
+                if (MinBalanceModel != MinBalanceModel.None)
                 {
-                    queryParameters.Add(Parameters.PhasingMinBalanceModel, ((int)MinBalanceModel.Value).ToString());
+                    queryParameters.Add(Parameters.PhasingMinBalanceModel, ((int)MinBalanceModel).ToString());
                 }
                 WhiteListed.ForEach(w => queryParameters.Add(Parameters.PhasingWhitelisted, w.AccountRs));
                 LinkedFullHash.ForEach(h => queryParameters.Add(Parameters.PhasingLinkedFullHash, h.ToHexString()));
