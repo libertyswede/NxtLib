308242a672b63a6aaa3710a875826503c8c1a8d0
Liberty Swede <libertyswede@outlook.com>
2016-09-21 23:42:11 +0200

    Bugfix, when both account & currency is included in request, the response data will not be contained inside an array.

--- Src/NxtLib/MonetarySystem/GetAccountCurrenciesReply.cs
+++ Src/NxtLib/MonetarySystem/GetAccountCurrenciesReply.cs
@@ -2,8 +2,12 @@
 
 namespace NxtLib.MonetarySystem
 {
-    public class GetAccountCurrenciesReply : BaseReply
+    public class GetAccountCurrenciesReply : AccountCurrency, IBaseReply
     {
         public List<AccountCurrency> AccountCurrencies { get; set; }
+        public IEnumerable<KeyValuePair<string, string>> PostData { get; set; }
+        public string RawJsonReply { get; set; }
+        public int RequestProcessingTime { get; set; }
+        public string RequestUri { get; set; }
     }
 }
