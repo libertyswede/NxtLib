1fd8bb65346d1344dbe099c405c164c2abd457d4
Liberty Swede <libertyswede@outlook.com>
2016-09-28 13:01:15 +0200

    Harmonized the api to existing code

---
+++ Src/NxtLib/Internal/AccountCurrencyConverter.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NxtLib.MonetarySystem;
+using System;
+
+namespace NxtLib.Internal
+{
+    internal class AccountCurrencyConverter : JsonConverter
+    {
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType != JsonToken.StartObject)
+            {
+                throw new ArgumentException("Invalid token type, must be StartObject");
+            }
+            if (objectType != typeof(GetAccountCurrenciesReply))
+            {
+                throw new ArgumentException("Can only convert to GetAccountCurrenciesReply");
+            }
+            var accountCurrenciesReply = new GetAccountCurrenciesReply();
+            var jObject = JObject.Load(reader);
+
+            var accountCurrencies = jObject.SelectToken(Parameters.AccountCurrencies) as JArray;
+            if (accountCurrencies != null)
+            {
+                foreach (var accountCurrencyJson in accountCurrencies)
+                {
+                    var accountCurrency = JsonConvert.DeserializeObject<AccountCurrency>(accountCurrencyJson.ToString());
+                    accountCurrenciesReply.AccountCurrencies.Add(accountCurrency);
+                }
+            }
+            else
+            {
+                var accountCurrency = JsonConvert.DeserializeObject<AccountCurrency>(jObject.ToString());
+                accountCurrenciesReply.AccountCurrencies.Add(accountCurrency);
+            }
+
+            return accountCurrencies;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
